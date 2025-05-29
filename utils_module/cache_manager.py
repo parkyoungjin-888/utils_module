857c8f4c43374a95c247bb93c130acccbc0c4f1d
parkyoungjin-888 <pyj8813@gmail.com>
2025-05-29 18:31:58 +0900

    이름 및 변수명 수정

--- utils_module/cache_manager.py
+++ utils_module/cache_manager.py
@@ -4,16 +4,16 @@ from botocore.exceptions import ClientError
 from typing import Optional, Tuple
 
 
-class CacheClassManager:
+class CacheManager:
     def __init__(self, s3_client, bucket: str, file_cache_dir: str = "./tmp/cache"):
         self.s3_client = s3_client
         self.bucket = bucket
         self.file_cache_dir = file_cache_dir
         os.makedirs(self.file_cache_dir, exist_ok=True)
-        self._class_cache = {}
+        self._cache = {}
 
     def _get_remote_version(self, file_name: str) -> str:
-        response = self.s3_client.head_class(Bucket=self.bucket, Key=file_name)
+        response = self.s3_client.head_object(Bucket=self.bucket, Key=file_name)
         version = response.get('VersionId') if response else None
         return version
 
@@ -35,7 +35,7 @@ class CacheClassManager:
         cached_version = self._get_cached_version(file_name)
         if version is None and cached_version == remote_version:
             return local_path, cached_version
-        
+
         extra_args = {}
         if version is not None:
             extra_args['VersionId'] = version
@@ -51,9 +51,9 @@ class CacheClassManager:
                 raise FileNotFoundError(f"{file_name} is not found in S3")
             raise
 
-    def _load_class(self, file_name: str, class_name: str, version: Optional[str] = None):
+    def _load_obj(self, file_name: str, obj_name: str, version: Optional[str] = None):
         local_path = os.path.join(self.file_cache_dir, file_name)
-        self._download_from_s3(version)
+        self._download_from_s3(file_name, version)
 
         try:
             cache_dir = os.path.dirname(local_path)
@@ -62,15 +62,14 @@ class CacheClassManager:
                 sys.path.append(cache_dir)
             module_name = os.path.splitext(os.path.basename(file_name))[0]
             module = importlib.import_module(module_name)
-            class_obj = getattr(module, class_name)
-            class_key = f'{file_name}/{class_name}'
-            self._class_cache[class_key] = class_obj
+            _obj = getattr(module, obj_name)
+            obj_key = f'{file_name}/{obj_name}'
+            self._cache[obj_key] = _obj
         except Exception as e:
-            raise ImportError(f"Failed to load {class_name} from {file_name}: {str(e)}")
+            raise ImportError(f"Failed to load {obj_name} from {file_name}: {str(e)}")
 
-    def get_class(self, file_name: str, class_name: str, version: Optional[str] = None):
-        class_key = f'{file_name}/{class_name}'
-        if class_key not in self._class_cache:
-            self._load_class(file_name, class_name, version)
-        return self._class_cache[class_key]
-        
+    def get_obj(self, file_name: str, obj_name: str, version: Optional[str] = None):
+        obj_key = f'{file_name}/{obj_name}'
+        if obj_key not in self._cache:
+            self._load_class(file_name, obj_name, version)
+        return self._cache[obj_key]
